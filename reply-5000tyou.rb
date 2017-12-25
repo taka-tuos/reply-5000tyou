a0fef7d8cf801b51ecadc82994bdc9f885de72b8
taka-tuos <hvcnes@gmail.com>
2017-12-25 13:13:30 +0900

    3.6対応

--- reply-5000tyou.rb
+++ reply-5000tyou.rb
@@ -1,74 +1,75 @@
 # -*- coding: utf-8 -*-
 
-Plugin.create(:d250g2_emoji) do
-  command(:reply_5000tyou_post,
-          name: '5000兆円欲しい！',
-          condition: Plugin::Command[:CanReplyAll],
-          visible: true,
-          role: :timeline) do |m|
-    m.messages.map do |msg|
-      emoji(msg.message,'5000tyou.png')
-    end
-  end
-  
-  command(:reply_mori_post,
-          name: 'うんこもりもり森鴎外',
-          condition: Plugin::Command[:CanReplyAll],
-          visible: true,
-          role: :timeline) do |m|
-    m.messages.map do |msg|
-      emoji(msg.message,'mori.png')
-    end
-  end
-  
-  command(:reply_otintin_open_post,
-          name: 'おちんちんランド開園',
-          condition: Plugin::Command[:CanReplyAll],
-          visible: true,
-          role: :timeline) do |m|
-    m.messages.map do |msg|
-      emoji(msg.message,'otintin_open.jpg')
-    end
-  end
-  
-  command(:reply_otintin_close_post,
-          name: 'おちんちんランド閉園',
-          condition: Plugin::Command[:CanReplyAll],
-          visible: true,
-          role: :timeline) do |m|
-    m.messages.map do |msg|
-      emoji(msg.message,'otintin_close.jpg')
-    end
-  end
-  
-  command(:reply_otintin_opose_post,
-          name: 'おちんちんランド開閉',
-          condition: Plugin::Command[:CanReplyAll],
-          visible: true,
-          role: :timeline) do |m|
-    m.messages.map do |msg|
-      emoji(msg.message,'otintin_opose.jpg')
-    end
-  end
-  
-  command(:reply_final_post,
-          name: '最終形態',
-          condition: Plugin::Command[:CanReplyAll],
-          visible: true,
-          role: :timeline) do |m|
-    m.messages.map do |msg|
-      emoji(msg.message,'final.jpg')
-    end
-  end
+Plugin.create(:reply_5000tyou_post) do
+	command(:reply_5000tyou_post,
+					name: '5000兆円欲しい！',
+					condition: Plugin::Command[:CanReplyAll],
+					visible: true,
+					role: :timeline) do |m|
+		m.messages.map do |msg|
+			emoji(msg.message,'5000tyou.png')
+		end
+	end
+	
+	command(:reply_mori_post,
+					name: 'うんこもりもり森鴎外',
+					condition: Plugin::Command[:CanReplyAll],
+					visible: true,
+					role: :timeline) do |m|
+		m.messages.map do |msg|
+			emoji(msg.message,'mori.png')
+		end
+	end
+	
+	command(:reply_otintin_open_post,
+					name: 'おちんちんランド開園',
+					condition: Plugin::Command[:CanReplyAll],
+					visible: true,
+					role: :timeline) do |m|
+		m.messages.map do |msg|
+			emoji(msg.message,'otintin_open.jpg')
+		end
+	end
+	
+	command(:reply_otintin_close_post,
+					name: 'おちんちんランド閉園',
+					condition: Plugin::Command[:CanReplyAll],
+					visible: true,
+					role: :timeline) do |m|
+		m.messages.map do |msg|
+			emoji(msg.message,'otintin_close.jpg')
+		end
+	end
+	
+	command(:reply_otintin_opose_post,
+					name: 'おちんちんランド開閉',
+					condition: Plugin::Command[:CanReplyAll],
+					visible: true,
+					role: :timeline) do |m|
+		m.messages.map do |msg|
+			emoji(msg.message,'otintin_opose.jpg')
+		end
+	end
+	
+	command(:reply_final_post,
+					name: '最終形態',
+					condition: Plugin::Command[:CanReplyAll],
+					visible: true,
+					role: :timeline) do |m|
+		m.messages.map do |msg|
+			emoji(msg.message,'final.jpg')
+		end
+	end
 
-  def emoji(m,f)
-    id = m.idname
-    message = "@#{id}"
-    Thread.new {
-      m.post(message: message,
-             mediaiolist: [File.new(File.join(__dir__, 'image', f))])
-    }.trap { |err|
-      error err
-    }
-  end
+	def emoji(m,f)
+		world, = Plugin.filtering(:world_current, nil)
+		id = m.idname
+		message = ""
+		compose(world, body: message, to: m, mediaiolist: [File.new(File.join(__dir__, 'image', f))])
+					.next { |tweet_message|
+						notice tweet_message
+					} .trap { |err|
+						error err
+					}
+	end
 end
